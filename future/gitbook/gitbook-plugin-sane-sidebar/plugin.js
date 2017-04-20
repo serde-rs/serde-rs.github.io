b0b8e35189967ea43179fddb302a8ee9da3e15ec
David Tolnay <dtolnay@gmail.com>
2017-04-20 08:06:48 -0700

    1.0 website

--- future/gitbook/gitbook-plugin-sane-sidebar/plugin.js
+++
@@ -1,7 +0,0 @@
-require(["gitbook", "jQuery"], function(gitbook, $) {
-    gitbook.events.bind('start', function (e, config) {
-        if ($(document).width() <= 600) {
-            gitbook.sidebar.toggle(false, false);
-        }
-    });
-});
