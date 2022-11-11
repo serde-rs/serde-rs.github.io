52c3bd09b711366c5a19e3e986253b74652b5850
David Tolnay <dtolnay@gmail.com>
2022-11-10 21:45:35 -0800

    Migrate Universal Analytics to Google Analytics 4

---
+++ gitbook/gitbook-plugin-ga4/plugin.js
@@ -0,0 +1,22 @@
+require(["gitbook"], function(gitbook) {
+    gitbook.events.bind("start", function(e, config) {
+        window.dataLayer = window.dataLayer || [];
+        window.gtag = window.gtag || function() {
+            window.dataLayer.push(arguments);
+        };
+        window.gtag('js', new Date());
+        window.gtag('config', config.ga4.tag, {
+            'anonymize_ip': config.ga4.anonymize_ip ?? false
+        });
+        var script = document.createElement("script");
+        script.type = "text/javascript";
+        script.async = true;
+        script.src = "https://www.googletagmanager.com/gtag/js?id=" + config.ga4.tag;
+        document.getElementsByTagName("head")[0].appendChild(script);
+    });
+    gitbook.events.bind("page.change", function() {
+        window.gtag('event', 'page_view', {
+            'page_location': window.location.pathname+window.location.search
+        });
+    });
+});
